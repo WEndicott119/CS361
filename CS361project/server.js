9588bea7f01558f868a5bf4f825999840ed2086b
William Endicott <endicotw@oregonstate.edu>
2021-05-15 17:14:35 -0700

    add

--- CS361project/server.js
+++ CS361project/server.js
@@ -1,35 +1,51 @@
 const express = require("express");
 const app = express()
 app.set('port', 3547);
+const fs = require('fs');
 
+const cheerio = require("cheerio");
 const axios = require('axios');
 const htmlParser = require("node-html-parser");
 
 app.use(express.static(__dirname + '/views'));
 
 
+
  axios.get('https://en.wikipedia.org/wiki/Justin_Herbert')
        .then(response => {
+      
+	const html = htmlParser.parse(response.data);
+	const pageContent = html.querySelector(".mw-parser-output");
 
-        const html = htmlParser.parse(response.data);
-        const pageContent = html.querySelector(".mw-parser-output");
-
-        const pElements = pageContent.querySelectorAll("p");
+	const pElements = pageContent.querySelectorAll("p");
 
 
 
-        console.log(pElements[2].rawText);
-       })
+	const elements = (pElements[2].rawText);
+	fs.writeFileSync('scrape.json', elements);
 
-.catch((error) => {
-    console.log(error);
-  });
+	document.body.innerHTML = elements;
+
+        var textString = fs.readFileSync('scrape.json');
+        var note = JSON.parse(textString);
+
+	console.log(pElements[1].rawText);
+	console.log(pElements[2].rawText);
+       	})
+
+	.catch((error) => {
+    	console.log(error);
+    	});
+
 
 
 
 app.get('/',function(req,res){
 res.status(200).sendFile(__dirname + '/views/index.html');
+
+
 });
+
 
 app.get('*', function(req,res){
   res.status(404).sendFile(__dirname + '/views/404.html');
@@ -39,3 +55,4 @@ app.get('*', function(req,res){
 // listen on port specified with node index.js XXXX
 app.listen(app.get('port'), () =>{
 console.log("LISTENING ON PORT 3000!")});
+
